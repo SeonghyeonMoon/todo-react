6b66b4c567ae6a3959683ea833a2d38ce379304b
Sherty <shine7329@gmail.com>
2021-11-12 19:44:35 +0900

    기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
+import { useCallback, useRef, useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Content 1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Content 2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Content 3',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
   return (
     <div className="App">
       <TodoTemplate>
-        <TodoInsert />
-        <TodoList/>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList
+          todos={todos}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       </TodoTemplate>
     </div>
   );
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,17 +1,22 @@
-import {   MdCheckBoxOutlineBlank,   MdRemoveCircleOutline, } from 'react-icons/md'
-import './TodoListItem.scss'
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+import './TodoListItem.scss';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
   return (
-      <div className="TodoListItem">
-        <div className="checkbox">
-          <MdCheckBoxOutlineBlank />
-          <div className="text">할 일</div>
-        </div>
-        <div className="remove">
-          <MdRemoveCircleOutline />
-        </div>
+    <div className="TodoListItem">
+      <div className={'checkbox' + (checked ? ' checked' : '')} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
   );
 };
 
